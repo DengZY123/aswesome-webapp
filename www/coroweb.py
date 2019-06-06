3ab1a0f2d228be3c3fa22ccf8be884d7e006963c
edz <458548791@qqj.com>
2019-06-06 18:05:57 +0800

    add handlers.py

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,9 +1,14 @@
 import asyncio
 import functools
+import inspect
 import logging
+import os
 from urllib import parse
 
 from aiohttp import web
+
+from www.apis import APIError
+
 
 
 def get(path):
@@ -37,13 +42,13 @@ class RequestHandler(object):
         self._app = app
         self.__func = fn
 
-    def __call__(self, request):
+    async def __call__(self, request):
         kw = None
 
-        if   self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
-        if request.method == 'POST':
-            if not request.content_type:
-                return web.HTTPBadRequest('Missing Content-Type.')
+        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing Content-Type.')
             ct = request.content_type.lower()
             if ct.startswith('application/json'):
                 params = await request.json()
@@ -91,10 +96,15 @@ class RequestHandler(object):
         except APIError as e:
             return dict(error=e.error, data=e.data, message=e.message)
 
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static/', path))
+
 def add_route(app,fn):
     method = getattr(fn,'__method__',None)
     path = getattr(fn,'__route__',None)
-    if path in None or method is None:
+    if path is None or method is None:
         raise ValueError("@get or  @post not defined in %s." % str(fn))
     if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
         fn = asyncio.coroutine(fn)
@@ -102,13 +112,13 @@ def add_route(app,fn):
         'add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
     app.router.add_route(method, path, RequestHandler(app, fn))
 
-def add_route(app,module_name):
+def add_routes(app,module_name):
     n = module_name.rfind('.')
     if n == (-1):
         mod = __import__(module_name,globals(),locals())
     else :
         name = module_name[n+1:]
-        mod =getattr(__import__(module_name[:n],globals(),locals(),[name],name))
+        mod =getattr(__import__(module_name[:n],globals(),locals(),[name]),name)
     for attr in dir(mod):
         if attr.startswith('_'):
             continue
